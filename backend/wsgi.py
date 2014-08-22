cd4e30eb78ff597b56a34fed2201793309b5a471
Lucas Amaral <lucasamaral1@gmail.com>
2014-08-22 18:46:02 -0300

    configurando database para funcionar localmente

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 WSGI config for backend project.
 
@@ -8,12 +9,13 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
-
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
+# configuração default do wsgi
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
 
 application = Cling(get_wsgi_application())
