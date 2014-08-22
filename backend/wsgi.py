51b2562d509d15063782cf2d7e4f72196569021d
Lucas Amaral <lucasamaral1@gmail.com>
2014-08-22 20:42:40 -0300

    arrumando ordem de imports no wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -9,13 +9,14 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 # configuração default do wsgi
 # from django.core.wsgi import get_wsgi_application
 # application = get_wsgi_application()
+# 
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 application = Cling(get_wsgi_application())
