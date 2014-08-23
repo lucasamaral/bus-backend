923467c584049d9206dbac0538e9e6bf5830efda
Lucas Amaral <lucasamaral1@gmail.com>
2014-08-23 18:47:52 -0300

    adicionando uma view em busapp

--- backend/urls.py
+++ backend/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'backend.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^busapp/', include('busapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
