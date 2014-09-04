987354c9c90a9b29c6fdd031f12f7bad2e210832
Lucas Amaral <lucasamaral1@gmail.com>
2014-09-03 21:18:50 -0300

    adicionando busline e buslinerelation

--- busapp/models.py
+++ busapp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Point(models.Model):
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     lon = models.DecimalField(max_digits=9, decimal_places=6)
+
     def __unicode__(self):
         return 'Lat:%011.6fLon:%011.6f' % (self.lat, self.lon)
 
@@ -12,12 +13,38 @@ class Stop(models.Model):
     name = models.CharField(blank=True, null=True, max_length=50)
     address = models.CharField(max_length=50)
     point = models.OneToOneField(Point)
+
     def __unicode__(self):
         return self.address
 
 
 class LineSegment(models.Model):
-    firstStop = models.ForeignKey(Stop, related_name="first_stop")
-    secondStop = models.ForeignKey(Stop, related_name="second_stop")
+    first_stop = models.ForeignKey(Stop, related_name="+")
+    second_stop = models.ForeignKey(Stop, related_name="+")
+
     def __unicode__(self):
-        return unicode(self.firstStop) + ' to ' + unicode(self.secondStop)
+        return unicode(self.first_stop) + ' to ' + unicode(self.second_stop)
+
+
+class BusLine(models.Model):
+    name = models.CharField(max_length=30)
+    number = models.CharField(max_length=20)
+    segments = models.ManyToManyField(LineSegment, through='BusLineRelation')
+
+    def get_start_segment(self):
+        return self.segments.objects.order_by('order').first()
+
+    def get_end_segment(self):
+        return self.segments.objects.order_by('order').last()
+
+    def __unicode__(self):
+        return self.number + '-' + self.name
+
+
+class BusLineRelation(models.Model):
+    bus_line = models.ForeignKey(BusLine)
+    line_segment = models.ForeignKey(LineSegment)
+    order = models.IntegerField()
+
+    def __unicode__(self):
+        return unicode(self.bus_line) + '#' + str(self.order)
