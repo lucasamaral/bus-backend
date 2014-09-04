0d077e97a206b63d32436208e6a021956a473ecf
lucasamaral <lucasamaral1@gmail.com>
2014-09-04 15:41:17 -0300

    Update tests.py

--- busapp/tests.py
+++ busapp/tests.py
@@ -11,8 +11,8 @@ class PointTestCase(TestCase):
     def test_unicode_works_point(self):
         point1 = Point.objects.get(Q(lat=0.0), Q(lon=0.0))
         point2 = Point.objects.get(Q(lat=90.0), Q(lon=0.0))
-        self.assertEqual(unicode(point1), 'Lat:0000.000000Lon:000.000000')
-        self.assertEqual(unicode(point2), 'Lat:0090.000000Lon:000.000000')
+        self.assertEqual(unicode(point1), 'Lat:0000.000000Lon:0000.000000')
+        self.assertEqual(unicode(point2), 'Lat:0090.000000Lon:0000.000000')
 
 class StopTestCase(TestCase):
     def setUp(self):
