e76cf20adbcc9bd574d411c21b9d955bbb8f742b
Lucas Amaral <lucasamaral1@gmail.com>
2014-11-07 01:33:02 -0200

    adding command to update estimated time'

---
+++ busapp/management/commands/update_times.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+
+from busapp.models import BusLine, BusLineRelation
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        all_lines = BusLine.objects.all()
+        relations = BusLineRelation.objects.filter(bus_line__in=all_lines)
+        for rel in relations:
+            times = rel.line_segment.timemeasured_set
+            average = reduce(lambda x, y: x.time_value + y.time_value)/len(times)
+            rel.line_segment.time_estimated = int(average)
+            rel.line_segment.save()
